fbeff5e7274018781a7f8a5bf136dda03bf926b6
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-06-24 22:01:35 -0600

    Initialize the sum of digits program

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number,sum=0,m;
+            Console.WriteLine("Enter a four digit number");
+            number = int.Parse(Console.ReadLine());
+
+            while(number>0)
+            {
+                m = number % 10;
+                sum = sum + m;
+                number = number/10;
+            }
+
+            Console.WriteLine("The Sum of the digits {0} is {1}", number, sum);
+
+
+        }
+    }
+}
